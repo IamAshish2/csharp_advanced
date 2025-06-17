22ddaaee99a6e1132adcfd8938826d2bc0dec330
IamAshish2 <karkiaashish9899@gmail.com>
2025-06-17 22:28:44 +0545

    add SignalR for real time connection and establish initial hub

---
+++ asp dotnet core brushup/SignalR/backend_webapi/Signal_R/Signal_R/Data/SharedDb.cs
@@ -0,0 +1,11 @@
+﻿using Signal_R.Models;
+using System.Collections.Concurrent;
+
+namespace Signal_R.Data
+{
+    public class SharedDb
+    {
+        private readonly ConcurrentDictionary<string, UserConnection> _connections = new ConcurrentDictionary<string, UserConnection>();
+        public  ConcurrentDictionary<string, UserConnection> Connections => _connections;
+    }
+}
---
+++ asp dotnet core brushup/SignalR/backend_webapi/Signal_R/Signal_R/Hubs/MessageHub.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Signal_R.Data;
+using Signal_R.Models;
+
+namespace Signal_R.Hubs
+{
+    public class MessageHub : Hub
+    {
+        private readonly SharedDb _sharedDb;
+        public MessageHub(SharedDb sharedDb) => _sharedDb = sharedDb;
+        
+        public async Task JoinChatRoomAsync(UserConnection conn)
+        {
+            await Clients.All
+                .SendAsync("ReceiveMessage","admin",$"The user {conn.UserName} has joined the chatRoom.");
+        }
+
+        public Task SendMessageToAllAsync(string message)
+        {
+            return Clients.All.SendAsync("ReceiveMessage", message);
+        }
+
+        public async Task JoinSpecificChatRoom(UserConnection conn)
+        {
+            // add the user's connectionId to the chatRoom
+            await Groups.AddToGroupAsync(Context.ConnectionId, conn.ChatRoom);
+
+            // store the connection in db
+            _sharedDb.Connections[Context.ConnectionId] = conn;
+
+            // specify the send back message to the group.
+            await Clients.Group(conn.ChatRoom).SendAsync("ReceiveMessage", "admin", $"The user {conn.UserName} has joined the {conn.ChatRoom}.");
+        }
+
+        public async Task SendMessageAsync(string message)
+        {
+            if (_sharedDb.Connections.TryGetValue(Context.ConnectionId, out UserConnection conn))
+            {
+                await Clients.Group(conn.ChatRoom)
+                       .SendAsync("ReceiveSpecificMessage", conn.UserName, message);
+            }
+        }
+    }
+}
